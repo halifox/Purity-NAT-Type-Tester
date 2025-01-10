9e27d70dbea15bd12685ae71e9aa0a7f5a621e65
halifox <106858061+halifox@users.noreply.github.com>
2025-01-10 19:34:27 +0800

    Rename NAT mapping and filtering enums for improved clarity and consistency.

--- lib/check_v2.dart
+++ lib/check_v2.dart
@@ -15,11 +15,11 @@ StunClient _stunClient = StunClient.create(
   serverHost: "stun.hot-chilli.net",
   serverPort: 3478,
   localIp: "0.0.0.0",
-  localPort: 54320,
+  localPort: 54322,
   stunProtocol: StunProtocol.RFC5780,
 );
 
-enum NatMapping {
+enum NatMappingBehavior {
   Block,
   EndpointIndependent,
   AddressDependent,
@@ -30,9 +30,8 @@ Future<void> testMappingPhase() async {
   await _initializeLocalAddresses();
   var mb = await _testMappingPhase1();
   print(mb);
-  var fb =  await _testFilteringPhase1();
+  var fb = await _testFilteringPhase1();
   print(fb);
-
 }
 
 _initializeLocalAddresses() async {
@@ -43,7 +42,7 @@ _initializeLocalAddresses() async {
   }
 }
 
-Future<NatMapping> _testMappingPhase1() async {
+Future<NatMappingBehavior> _testMappingPhase1() async {
   try {
     //1）客户端A以IP_CA: PORT_CA给STUN Server的IP_SA: PORT_SA发送一个bind请求，STUN server以IP_SA: PORT_SA给客户端A的IP_CA: PORT_CA回复响应，
     // 响应内容大体为：（NAT映射后的IP地址和端口为：IP_MCA1: PORT_MCA1，STUN Server的另外一个IP地址和端口为：IP_SB: PORT_SB）。
@@ -51,18 +50,18 @@ Future<NatMapping> _testMappingPhase1() async {
     StunMessage message = await _stunClient.sendAndAwait(_stunClient.createBindingStunMessage());
     _stunClient.disconnect();
     if (_localAddresses.contains(message.xorMappedAddressAttribute.addressDisplayName) && message.xorMappedAddressAttribute.port == 3478) {
-      return NatMapping.EndpointIndependent;
+      return NatMappingBehavior.EndpointIndependent;
     }
     return _testMappingPhase2(message);
   } on TimeoutException catch (e) {
-    return NatMapping.Block;
+    return NatMappingBehavior.Block;
   } catch (e) {
     print(e);
     rethrow;
   }
 }
 
-Future<NatMapping> _testMappingPhase2(StunMessage message1) async {
+Future<NatMappingBehavior> _testMappingPhase2(StunMessage message1) async {
   try {
     // 2）客户端A以IP_CA: PORT_CA给STUN server的IP_SB: PORT_SA(相对步骤1 ip改变了)发送一个bind请求，STUN server以IP_SB: PORT_SA给客户端A的IP_CA: PORT_CA回复响应，
     // 响应内容大体为：（NAT映射后的IP地址和端口为：IP_MCA2: PORT_MCA2）。
@@ -73,18 +72,18 @@ Future<NatMapping> _testMappingPhase2(StunMessage message1) async {
     StunMessage message = await _stunClient.sendAndAwait(_stunClient.createBindingStunMessage());
     _stunClient.disconnect();
     if (message.xorMappedAddressAttribute == message1.xorMappedAddressAttribute) {
-      return NatMapping.EndpointIndependent;
+      return NatMappingBehavior.EndpointIndependent;
     }
     return _testMappingPhase3(message1, message);
   } on TimeoutException catch (e) {
-    return NatMapping.Block;
+    return NatMappingBehavior.Block;
   } catch (e) {
     print(e);
     rethrow;
   }
 }
 
-Future<NatMapping> _testMappingPhase3(StunMessage message1, StunMessage message2) async {
+Future<NatMappingBehavior> _testMappingPhase3(StunMessage message1, StunMessage message2) async {
   try {
     // 3）客户端A以IP_CA: PORT_CA给STUN server的IP_SB: PORT_SB(相对步骤1 ip和port改变了)发送一个bind请求，STUN server以IP_SB: PORT_SB给客户端A的IP_CA: PORT_CA回复响应，
     // 响应内容大体为：（NAT映射后的IP地址和端口为：IP_MCA3: PORT_MCA3）。
@@ -96,35 +95,35 @@ Future<NatMapping> _testMappingPhase3(StunMessage message1, StunMessage message2
     StunMessage message = await _stunClient.sendAndAwait(_stunClient.createBindingStunMessage());
     _stunClient.disconnect();
     if (message.xorMappedAddressAttribute == message2.xorMappedAddressAttribute) {
-      return NatMapping.AddressDependent;
+      return NatMappingBehavior.AddressDependent;
     } else {
-      return NatMapping.AddressAndPortDependent;
+      return NatMappingBehavior.AddressAndPortDependent;
     }
   } on TimeoutException catch (e) {
-    return NatMapping.Block;
+    return NatMappingBehavior.Block;
   } catch (e) {
     print(e);
     rethrow;
   }
 }
 
-enum Filtering {
+enum NatFilteringBehavior {
   Block,
   EndpointIndependent,
   AddressDependent,
   AddressAndPortDependent,
 }
 
-Future<Filtering> _testFilteringPhase1() async {
+Future<NatFilteringBehavior> _testFilteringPhase1() async {
   try {
     // 4）客户端A以IP_CA: PORT_CA给STUN server的IP_SA: PORT_SA发送一个bind请求（请求中带CHANGE-REQUEST attribute来要求stun server改变IP和PORT来响应），STUN server以IP_SB: PORT_SB给客户端A的IP_CA: PORT_CA回复响应。
     // 如果客户端A能收到STUN server的响应，那么NAT是Endpoint-Independent Filtering的过滤规则，也就是只要给客户端A的IP_CA: PORT_CA映射后的IP_MCA: PORT_MCA地址发送数据都能通过NAT到达客户端A的IP_CA: PORT_CA（这种过滤规则的NAT估计很少）。
     // 如果不能收到STUN server的响应，那么需要进行下面的第五步测试。
     var createBindingStunMessage = _stunClient.createBindingStunMessage();
-    createBindingStunMessage.attributes.add(rfc5780.ChangeRequest(StunAttributes.TYPE_CHANGE_REQUEST, 8, true, true));
+    createBindingStunMessage.attributes.add(rfc5780.ChangeRequest(flagChangeIp: true, flagChangePort: true));
     StunMessage message = await _stunClient.sendAndAwait(createBindingStunMessage);
     _stunClient.disconnect();
-    return Filtering.EndpointIndependent;
+    return NatFilteringBehavior.EndpointIndependent;
   } on TimeoutException catch (e) {
     return _testFilteringPhase2();
   } catch (e) {
@@ -133,7 +132,7 @@ Future<Filtering> _testFilteringPhase1() async {
   }
 }
 
-Future<Filtering> _testFilteringPhase2() async {
+Future<NatFilteringBehavior> _testFilteringPhase2() async {
   try {
     // 5）客户端A以IP_CA: PORT_CA给STUN server的IP_SA: PORT_SA发送一个bind请求（请求中带CHANGE-REQUEST attribute来要求stun server改变PORT来响应），STUN server以IP_SA: PORT_SB给客户端A的IP_CA: PORT_CA回复响应。
     // 如果客户端A能收到STUN server的响应，NAT是Address-Dependent Filtering的过滤规则，也就是只要之前客户端A以IP_CA: PORT_CA给IP为IP_D的主机发送过数据，
@@ -141,12 +140,12 @@ Future<Filtering> _testFilteringPhase2() async {
     // 如果不能收到响应，NAT是Address and Port-Dependent Filtering的过滤规则，也即是只有之前客户端A以IP_CA: PORT_CA给目的主机的IP_D: PORT_D发送过数据，
     // 那么在NAT映射的有效期内，只有以IP_D: PORT_D给客户端A的IP_CA: PORT_CA映射后的IP_MCA: PORT_MCA地址发送数据才能通过NAT到达客户端A的IP_CA: PORT_CA。
     var createBindingStunMessage = _stunClient.createBindingStunMessage();
-    createBindingStunMessage.attributes.add(rfc5780.ChangeRequest(StunAttributes.TYPE_CHANGE_REQUEST, 8, false, true));
+    createBindingStunMessage.attributes.add(rfc5780.ChangeRequest(flagChangeIp: false, flagChangePort: true));
     StunMessage message = await _stunClient.sendAndAwait(createBindingStunMessage);
     _stunClient.disconnect();
-    return Filtering.AddressDependent;
+    return NatFilteringBehavior.AddressDependent;
   } on TimeoutException catch (e) {
-    return Filtering.AddressAndPortDependent;
+    return NatFilteringBehavior.AddressAndPortDependent;
   } catch (e) {
     print(e);
     rethrow;
