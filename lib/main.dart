cdd2e57eb0075b9f0c71e164b6349dae5ee6bf64
halifox <106858061+halifox@users.noreply.github.com>
2025-01-13 23:00:43 +0800

    Refactored NAT type detection with event bus.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:stun/src/nat_checker_rfc_5780.dart' as rfc5780;
+import 'package:purity_nat_type_tester/checker_event.dart';
+
+import 'event_bus.dart';
+import 'nat_checker_rfc_5780.dart';
 
 void main() {
   runApp(const MyApp());
@@ -45,8 +48,103 @@ class _HomePageState extends State<HomePage> {
   final String pattern = r'^([a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+):([1-9][0-9]{0,4})$';
   TextEditingController stunController = TextEditingController(text: "stun.hot-chilli.net:3478");
 
-  rfc5780.NatMappingBehavior? natMappingBehavior;
-  rfc5780.NatFilteringBehavior? natFilteringBehavior;
+  NatMappingBehavior? natMappingBehavior;
+  NatFilteringBehavior? natFilteringBehavior;
+
+  CheckerEvent event = CheckerEvent();
+  bool isRunning = false;
+
+  String get natMappingBehaviorMessage {
+    return switch (natMappingBehavior) {
+      null => "等待",
+      NatMappingBehavior.Block => "Block",
+      NatMappingBehavior.EndpointIndependent => "Endpoint-Independent Mapping",
+      NatMappingBehavior.AddressDependent => "Address and Port-Dependent Mapping",
+      NatMappingBehavior.AddressAndPortDependent => "Address-Dependent Mapping",
+    };
+  }
+
+  String get natFilteringBehaviorMessage {
+    return switch (natFilteringBehavior) {
+      null => "等待",
+      NatFilteringBehavior.Block => "Block",
+      NatFilteringBehavior.EndpointIndependent => "Endpoint-Independent Filtering",
+      NatFilteringBehavior.AddressDependent => "Address and Port-Dependent Filtering",
+      NatFilteringBehavior.AddressAndPortDependent => "Address-Dependent Filtering",
+    };
+  }
+
+  String get rfc3489Message {
+    if (natMappingBehavior == NatMappingBehavior.EndpointIndependent && natFilteringBehavior == NatFilteringBehavior.EndpointIndependent) {
+      return "对应于RFC3489中的Full Cone NAT";
+    }
+    if (natMappingBehavior == NatMappingBehavior.EndpointIndependent && natFilteringBehavior == NatFilteringBehavior.AddressDependent) {
+      return "对应于RFC3489中的Restricted Cone NAT";
+    }
+    if (natMappingBehavior == NatMappingBehavior.EndpointIndependent && natFilteringBehavior == NatFilteringBehavior.AddressAndPortDependent) {
+      return "对应于RFC3489中的Port Restricted Cone NAT";
+    }
+    if (natMappingBehavior == NatMappingBehavior.AddressAndPortDependent && natFilteringBehavior == NatFilteringBehavior.AddressAndPortDependent) {
+      return "对应于RFC3489中的Symmetric NAT";
+    }
+    return "不对应于RFC3489中任何类型;RFC3489只描述了9种NAT组合行为类型中的4种。";
+  }
+
+  String get message {
+    return switch (event.result) {
+      EventState.idle => "等待",
+      EventState.running => "运行中",
+      EventState.success => "结果:\n映射规则:${natMappingBehaviorMessage}\n过滤规则:${natFilteringBehaviorMessage}\n${rfc3489Message}",
+      EventState.timeout => "超时",
+      EventState.error => "错误",
+      EventState.never => "异常",
+    };
+  }
+
+  @override
+  void initState() {
+    bus.stream.listen((event) {
+      if (event is CheckerEvent) {
+        setState(() {
+          this.event = event;
+        });
+      }
+    });
+    super.initState();
+  }
+
+  Widget StateIcon(EventState state) {
+    return switch (state) {
+      EventState.idle => Icon(Icons.trip_origin),
+      EventState.running => Icon(Icons.timelapse),
+      EventState.success => Icon(Icons.check),
+      EventState.timeout => Icon(Icons.running_with_errors),
+      EventState.error => Icon(Icons.error_outline),
+      EventState.never => Icon(Icons.block),
+    };
+  }
+
+  Widget StateItem(EventState state, String message) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 4, horizontal: 0),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Theme.of(context).colorScheme.primaryContainer,
+        ),
+        child: Row(
+          children: [
+            StateIcon(state),
+            SizedBox(width: 8),
+            Expanded(
+              child: Text(message),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,14 +161,21 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("${natMappingBehavior}"),
-                Text("${natFilteringBehavior}"),
+                StateItem(event.initializeLocalAddresses, "本机网络接口检测"),
+                StateItem(event.performPhase1MappingTest, "本地公网地址检测"),
+                StateItem(event.performPhase2MappingTest, "独立端点映射检测"),
+                StateItem(event.performPhase3MappingTest, "地址依赖映射检测"),
+                StateItem(event.performPhase1FilteringTest, "独立端点过滤检测"),
+                StateItem(event.performPhase2FilteringTest, "地址依赖过滤检测"),
+                StateItem(event.result, message),
+                SizedBox(height: 8),
                 buildStunEditText(),
                 buildStunChipGroup(),
               ],
@@ -78,35 +183,49 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.play_arrow),
-        onPressed: () async {
-          try {
-            String input = stunController.text;
-
-            RegExpMatch? match = RegExp(pattern).firstMatch(input);
-            if (match == null) {
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("STUN 输入错误")));
-              return;
-            }
-
-            String host = match.group(1)!;
-            int port = int.parse(match.group(3) ?? "3478");
-            rfc5780.NatChecker checker = rfc5780.NatChecker(
-              serverHost: host,
-              serverPort: port,
-            );
-            var (natMappingBehavior, natFilteringBehavior) = await checker.check();
-            setState(() {
-              this.natMappingBehavior = natMappingBehavior;
-              this.natFilteringBehavior = natFilteringBehavior;
-            });
-          } catch (e) {
-            showErrorDialog(context, "${e}");
-          }
-        },
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(isRunning ? Icons.timelapse : Icons.play_arrow),
+        label: Text(isRunning ? "等待结果" : "开始检查"),
+        onPressed: startCheck,
       ),
     );
+  }
+
+  void startCheck() async {
+    if (isRunning) return;
+    try {
+      setState(() {
+        isRunning = true;
+      });
+      checkerEvent.clear();
+      updateCheckerEvent();
+
+      String input = stunController.text;
+
+      RegExpMatch? match = RegExp(pattern).firstMatch(input);
+      if (match == null) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("STUN 输入错误")));
+        return;
+      }
+
+      String host = match.group(1)!;
+      int port = int.parse(match.group(3) ?? "3478");
+      NatChecker checker = NatChecker(
+        serverHost: host,
+        serverPort: port,
+      );
+      var (natMappingBehavior, natFilteringBehavior) = await checker.check();
+      setState(() {
+        this.natMappingBehavior = natMappingBehavior;
+        this.natFilteringBehavior = natFilteringBehavior;
+      });
+    } catch (e) {
+      showErrorDialog(context, "${e}");
+    } finally {
+      setState(() {
+        isRunning = false;
+      });
+    }
   }
 
   void showErrorDialog(BuildContext context, String message) {
@@ -130,17 +249,14 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget buildStunEditText() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        controller: stunController,
-        decoration: InputDecoration(
-          labelText: "STUN",
-          border: const OutlineInputBorder(
-            borderSide: BorderSide(),
-            borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            gapPadding: 8.0,
-          ),
+    return TextField(
+      controller: stunController,
+      decoration: InputDecoration(
+        labelText: "STUN",
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(),
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          gapPadding: 8.0,
         ),
       ),
     );
